4a0edcf8cc2c82fde1399350cb4ce4f731f53564
melat-lema <169100419+melat-lema@users.noreply.github.com>
2025-10-02 14:05:38 +0300

    last update

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const axiosBase= axios.create({
-  baseURL: 'https://nola-d-server.onrender.com'
+  baseURL: 'https://nola-d-server.onrender.com/api'
 })
 export default axiosBase
